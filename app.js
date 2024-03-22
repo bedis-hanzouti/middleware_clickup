34473e8d42c59dce118c524e0627a69fb2941d2a
Badis Hanzouti <hanzoutib@gmail.com>
2024-03-22 19:54:37 +0100

    add route page

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ require("./config/dataBase/dbConfig");
 require("./config/jobs/getAllDataFromClickupCron");
 
 app.use(express.json({ limit: "20kb" }));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 const stream = fs.createWriteStream(path.join(__dirname, "logger.log"), {
   flags: "a",
 });
@@ -24,6 +25,56 @@ if (process.env.NODE_ENV === "development") {
   console.log(`mode: ${process.env.NODE_ENV}`);
 }
 app.use("/api/v1", apiRouter);
+app.get("/", (req, res) => {
+  figlet("Proxym Clickup ...", function (err, data) {
+    if (err) {
+      console.log(
+        "\x1b[31mErreur lors de la génération du texte ASCII :",
+        err,
+        "\x1b[0m"
+      );
+      return;
+    }
+
+    const nameText =
+      "<span style='color: white; font-weight: bold;font-size: 45px;'>Badis-Hanzouti</span>";
+
+    const figletStyle = "color: white; font-weight: bold";
+
+    const animatedData = data
+      .split("")
+      .map(
+        (char, index) =>
+          `<span class="char-${index}" style="opacity: 0;">${char}</span>`
+      )
+      .join("");
+
+    const output = `
+      <div style="text-align:center; position: fixed; top: 0; left: 0; width: 100%; height: 100%; overflow: hidden;">
+        <video autoplay muted loop style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); min-width: 100%; min-height: 100%;">
+          <source src="./uploads//Welcome to the Game - Hacking Alert.mp4" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+        <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+          <pre style="${figletStyle}" id="figletText">${animatedData}</pre>
+          <div>${nameText}</div>
+          
+         
+        </div>
+      </div>
+      <script>
+        const charElements = document.querySelectorAll('[class^="char-"]');
+        charElements.forEach((charElement, index) => {
+          setTimeout(() => {
+            charElement.style.opacity = 1;
+          }, index * 10); // Reduced delay for faster animation
+        });
+      </script>
+    `;
+
+    res.send(output);
+  });
+});
 
 try {
   app.listen(PORT, () => {
